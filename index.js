fa058340fa9cccc829b66683f4a90883ea9ad623
Muregz <fabian.murega@student.moringaschool.com>
2024-07-10 20:04:22 +0300

    master

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, query) {
+       const lowercaseQuery = query.toLowerCase();
+        return drivers.filter(driver => driver.toLowerCase() === lowercaseQuery);
+}
+function fuzzyMatch(drivers, query) {
+    const lowercaseQuery = query.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().startsWith(lowercaseQuery));
+}
+function matchName(drivers, name) { 
+    return drivers.filter(driver => driver.name === name);
+}
